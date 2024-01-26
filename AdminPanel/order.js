26f1cbf8f0038318c25dadd82f10567228b92aad
unknown <kanoujiya28501@gmail.com>
2024-01-26 19:54:06 +0530

    adding project mousemovement and admin panel

---
+++ AdminPanel/order.js
@@ -0,0 +1,20 @@
+const Order=[
+    {
+        productName: 'Javascript tutorial',
+        productNumber: '85743',
+        paymentStatus: 'Due',
+        status:'Pending'
+    },
+    {
+        productName: 'CSS Full Course',
+        productNumber: '897245',
+        paymentStatus: 'Refunded',
+        status:'Declined'
+    },
+    {
+        productName: 'Flex-Box tutorial',
+        productNumber: '36452',
+        paymentStatus: 'Paid',
+        status:'Active'
+    },
+]
